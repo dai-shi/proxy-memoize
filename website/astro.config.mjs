91f78ae9e05f76570bd7ac464f8da20cf5cc6c25
sash0017 <sajad.sharhani.0017@student.uu.se>
2023-05-01 23:18:30 +0200

    github pages

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // setup for github pages
+  site: 'https://pheno-agency.github.io',
+  base: '/proxy-memoize',
+});
