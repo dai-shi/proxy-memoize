ca51b44aede3cd564b2270b757a6685463897b54
Morteza-Mazrae <mor.mazrae@gmail.com>
2023-05-02 16:18:26 +0430

    layout

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
+import UnoCSS from 'unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
   // setup for github pages
   site: 'https://pheno-agency.github.io',
   base: '/proxy-memoize',
+  integrations: [
+    UnoCSS({
+      injectReset: true, // or a path to the reset file
+    }),
+  ],
 });
---
+++ website/uno.config.ts
@@ -0,0 +1,35 @@
+// uno.config.ts
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss';
+
+export default defineConfig({
+  rules: [[/^m-([\.\d]+)$/, ([_, num]) => ({ margin: `${num}px` })]],
+  shortcuts: [
+    // ...
+  ],
+  theme: {
+    colors: {
+      // ...
+    },
+  },
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons(),
+    presetTypography(),
+    presetWebFonts({
+      fonts: {
+        // ...
+      },
+    }),
+  ],
+  transformers: [transformerDirectives(), transformerVariantGroup()],
+});
