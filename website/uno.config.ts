1c3368230ed9a649422e87250be94fb58239999a
Morteza-Mazrae <mor.mazrae@gmail.com>
2023-05-03 01:08:44 +0430

    toggle theme

--- website/uno.config.ts
+++ website/uno.config.ts
@@ -9,6 +9,7 @@ import {
   transformerDirectives,
   transformerVariantGroup,
 } from 'unocss';
+// import url('https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500&display=swap');
 
 export default defineConfig({
   rules: [[/^m-([\.\d]+)$/, ([_, num]) => ({ margin: `${num}px` })]],
@@ -17,7 +18,15 @@ export default defineConfig({
   ],
   theme: {
     colors: {
-      // ...
+      green: '#0DA84B',
+      indigo: '#183F6E',
+      lightBlue: '#DCEDFF',
+      cyan: '#ECF5FF',
+      gray: '#8094AC',
+      // dark mode colors
+      black: '#2D2D2D',
+      fullBlack: '#1C1C1C',
+      lightGray: '#A6A6A6',
     },
   },
   presets: [
@@ -26,8 +35,17 @@ export default defineConfig({
     presetIcons(),
     presetTypography(),
     presetWebFonts({
+      provider: 'google', // default provider
+
       fonts: {
-        // ...
+        Dm: {
+          name: 'DM Sans',
+          weights: ['400', '500', '700'],
+        },
+        Roboto: {
+          name: 'Roboto',
+          weights: ['700', '500'],
+        },
       },
     }),
   ],
