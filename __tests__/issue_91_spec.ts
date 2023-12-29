474a5b19ac091b34cb6b402dd27902378299cdc6
daishi <daishi@axlight.com>
2023-12-30 00:03:34 +0900

    two test cases

--- __tests__/issue_91_spec.ts
+++ __tests__/issue_91_spec.ts
@@ -1,7 +1,34 @@
 import { memoize } from '../src/index';
 
-describe('issue #91', () => {
-  it('no extra calculations with nested memoized selectors', () => {
+describe('no extra calculations (#91)', () => {
+  it('nested memoized selectors with primitive value access', () => {
+    const reduxState = {
+      books: {
+        bookByName: {
+          bookPrice1: 50,
+          bookPrice2: 100,
+        },
+      },
+    };
+    let selectAllBooksCount = 0;
+    const selectAllBooks = memoize((state: typeof reduxState) => {
+      selectAllBooksCount += 1;
+      return Object.values(state.books.bookByName);
+    });
+    const selectSomeBooks = memoize((state: typeof reduxState) => {
+      const someBooks = selectAllBooks(state);
+      return someBooks.filter((price) => price === 100);
+    });
+    const selectBooks = memoize((state: typeof reduxState) => {
+      const allBooks = selectAllBooks(state);
+      const someBooks = selectSomeBooks(state);
+      return { allBooks, someBooks };
+    });
+    selectBooks(reduxState);
+    expect(selectAllBooksCount).toBe(1);
+  });
+
+  it('nested memoized selectors with object access', () => {
     const reduxState = {
       books: {
         bookByName: {
