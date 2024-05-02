934efaa964a7384bbd9844f7a556961f1ecfb14e
Daishi Kato <dai-shi@users.noreply.github.com>
2024-05-02 11:34:38 +0900

    fix: unused selectors (#97)
    * add failing test for #96
    
    * fix!!!!!!
    
    * early return
    
    * update CHANGELOG

---
+++ __tests__/issue_96_spec.ts
@@ -0,0 +1,39 @@
+import { memoize } from '../src/index';
+
+describe('unused selectors (#96)', () => {
+  it('nested memoized selectors with primitive value access', () => {
+    type State = {
+      ids: string[];
+      entities: {
+        [id: string]: { name: string, array: string[] };
+      };
+    };
+    const state1: State = {
+      ids: ['1'],
+      entities: {
+        1: { name: 'first', array: [] },
+      },
+    };
+    const state2: State = {
+      ids: ['1'],
+      entities: {
+        1: { name: 'first', array: ['changed'] },
+      },
+    };
+    const selectAll = memoize((state: State) => state.ids.map((id) => state.entities[id]));
+    const selectAllNames = memoize((state: State) => (
+      state.ids.map((id) => state.entities[id]?.name)
+    ));
+    const intermediateSelector = memoize((state: State) => {
+      selectAllNames(state);
+      return selectAll(state);
+    });
+    const finalSelector = memoize((state: State) => {
+      const combinedResult = intermediateSelector(state);
+      return combinedResult;
+    });
+    const result1 = finalSelector(state1);
+    const result2 = finalSelector(state2);
+    expect(result1).not.toEqual(result2);
+  });
+});
