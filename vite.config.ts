addcb80454fe9939f7ca3b826ec74f475d57f620
Daishi Kato <dai-shi@users.noreply.github.com>
2025-01-26 22:48:27 +0900

    chore(eslint): migrate to eslint 9 flat config (#107)
    * chore(eslint): migrate to eslint 9 flat config
    
    * update deps & eslint plugins
    
    * revert deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,26 @@
 
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+const { DIR, PORT = '8080' } = process.env;
 
 export default defineConfig(({ mode }) => {
   if (mode === 'test') {
     return {
-      resolve: { alias: { 'proxy-memoize': resolve('src') } },
+      test: {
+        environment: 'happy-dom',
+        setupFiles: ['./tests/vitest-setup.ts'],
+      },
+      plugins: [tsconfigPaths()],
     };
   }
-  return {};
+  if (!DIR) {
+    throw new Error('DIR environment variable is required');
+  }
+  return {
+    root: resolve('examples', DIR),
+    server: { port: Number(PORT) },
+    plugins: [tsconfigPaths({ root: resolve('.') })],
+  };
 });
